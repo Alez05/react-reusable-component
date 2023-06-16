260b098a9e9c9eceb0d169f866a17cb92b7e5d53
alez05 <cercel.alexandru436@gmail.com>
2023-06-16 20:23:16 +0100

    modal Fixes #28

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,4 +1,13 @@
-import { Aspect, Box, Button, Icon, Image, Input, Text } from '@myorg/atom'
+import {
+  Aspect,
+  Box,
+  Button,
+  Icon,
+  Image,
+  Input,
+  Modal,
+  Text,
+} from '@myorg/atom'
 
 const LandingPage = () => {
   return (
@@ -53,6 +62,10 @@ const LandingPage = () => {
             alt="ceva"
             src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Flandscapes&psig=AOvVaw0DFVfZDKgSFF7_3xZmc7_J&ust=1685806434903000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNiX_9f0pP8CFQAAAAAdAAAAABAR"
           />
+        </Box>
+
+        <Box justify="space-between" vertical color="danger">
+          <Modal>ceva frumos</Modal>
         </Box>
       </Aspect>
     </div>
---
+++ libs/atom/src/lib/modal/modal.type.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+export type TModal = {
+  children?: ReactNode
+  show?: boolean
+  closeModal?: () => void
+}
+export type $TModal = {
+  children?: ReactNode
+  show?: boolean
+}
